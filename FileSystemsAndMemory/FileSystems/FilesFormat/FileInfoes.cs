56379b19702f4bf344c86cb923e2cf24d8e48206
mkpbd <mostofakamal1990@gmail.com>
2023-05-29 16:05:04 +0600

    File Info

--- FileSystemsAndMemory/FileSystems/FilesFormat/FileInfoes.cs
+++ FileSystemsAndMemory/FileSystems/FilesFormat/FileInfoes.cs
@@ -47,5 +47,51 @@ namespace FileSystems.FilesFormat
         }
 
 
+        public void DeleteExistsFiles()
+        {
+
+            try
+            {
+                var fi1 = new FileInfo(path);
+                string path2 = Path.GetTempFileName();
+                var fi2 =  new     FileInfo(path2);
+                // Ensure that the target does not exist.
+                fi2.Delete();
+                // Copy the file.
+                fi1.CopyTo(path2);
+                Console.WriteLine(    $"{ path}  was copied to{ path2}.");
+                // Delete the newly created file.
+                fi2.Delete();
+                Console.WriteLine(
+                $"{ path2}   was successfully deleted.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(  $"The process failed: { e.ToString()} ");
+            }
+        }
+
+
+
+        public void FileInfoConstructor()
+        {
+            // Open an existing file, or create a new one.
+            FileInfo fi =
+            new
+            FileInfo( "temp.txt" );
+            // Create a writer, ready to add entries to the file.
+            StreamWriter sw = fi.AppendText();
+            sw.WriteLine(    "This is a new entry to add to the file"   );
+            sw.WriteLine(  "This is yet another line to add..."   );
+            sw.Flush();
+            sw.Close();
+            // Get the information out of the file and display it.
+            StreamReader sr =
+            new
+            StreamReader(fi.OpenRead());
+            while  (sr.Peek() !=  -1  )
+                Console.WriteLine(sr.ReadLine());
+        }
+
     }
 }
