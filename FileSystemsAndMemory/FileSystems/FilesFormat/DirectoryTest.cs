103cf63bb9345b1b8899783cf299591db343b424
mkpbd <mostofakamal1990@gmail.com>
2023-05-29 16:20:19 +0600

    update is

---
+++ FileSystemsAndMemory/FileSystems/FilesFormat/DirectoryTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystems.FilesFormat
+{
+    public class DirectoryTest
+    {
+
+        public void GetPathFileDirectory()
+        {
+            // Open an existing file, or create a new one.
+            FileInfo fi = new FileInfo("temp.txt");
+            // Determine the full path of the file just created.
+            DirectoryInfo di = fi.Directory;
+            // Figure out what other entries are in that directory.
+            FileSystemInfo[] fsi = di.GetFileSystemInfos();
+            Console.WriteLine("The directory '{0}' contains the following files and directories:", di.FullName);
+            // Print the names of all the files and subdirectories of that directory.
+            foreach (FileSystemInfo info in fsi)
+                Console.WriteLine(info.Name);
+        }
+
+
+
+        public byte[] OpenDataFile(string FileName)
+        {
+            // Check the FileName argument.
+            if
+            (FileName == null || FileName.Length == 0)
+            {
+                throw new ArgumentNullException("FileName");
+            }
+            // Check to see if the file exists.
+            FileInfo fInfo =
+            new
+            FileInfo(FileName);
+            // You can throw a personalized exception if
+            // the file does not exist.
+            if
+            (!fInfo.Exists)
+            {
+                throw new FileNotFoundException("The file was not found.", FileName);
+            }
+
+
+            // Open the file.
+            FileStream fStream = new FileStream(FileName, FileMode.Open);
+            // Create a buffer.
+            byte[] buffer = new byte[fStream.Length];
+            // Read the file contents to the buffer.
+            fStream.Read(buffer, 0, (int)fStream.Length);
+            // return the buffer.
+            return buffer;
+        }
+
+
+
+    }
+}
---
+++ FileSystemsAndMemory/FileSystems/FilesFormat/FileExample.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystems.FilesFormat
+{
+    public class FileExample
+    {
+        public static void GetFilesPath()
+        {
+            string FileName = @"c:\test.xml";
+            // Get the read-only value for a file.
+            bool isReadOnly = IsFileReadOnly(FileName);
+            // Display wether the file is read-only.
+            Console.WriteLine("The file read-only value for " + FileName + " is: " + isReadOnly);
+            Console.WriteLine("Changing the read-only value for " + FileName + " to true.");
+            // Set the file to read-only.
+            SetFileReadAccess(FileName, true);
+            // Get the read-only value for a file.
+            isReadOnly = IsFileReadOnly(FileName);
+            // Display that the file is read-only.
+            Console.WriteLine(
+            "The file read-only value for "
+            + FileName +
+            " is: "
+            + isReadOnly);
+        }
+        // Sets the read-only value of a file.
+        public static void SetFileReadAccess(string FileName, bool SetReadOnly)
+        {
+            // Create a new FileInfo object.
+            FileInfo fInfo =
+            new
+            FileInfo(FileName);
+            // Set the IsReadOnly property.
+            fInfo.IsReadOnly = SetReadOnly;
+        }
+        // Returns wether a file is read-only.
+        public static bool IsFileReadOnly(string FileName)
+        {
+            // Create a new FileInfo object.
+            // Create a new FileInfo object.
+            FileInfo fInfo = new FileInfo(FileName);
+
+            // Return the IsReadOnly property value.
+            return fInfo.IsReadOnly;
+        }
+    }
+
+}
